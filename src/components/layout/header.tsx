6ccfa0a5809bd04664022e5635cd269cbdbe4929
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2024-08-14 17:09:35 -0400

    feat: changed bg-white of header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -12,7 +12,7 @@ export function Header() {
 
   return (
     <header
-      className="sticky top-0 rounded-tl-md rounded-tr-md z-40 w-full backdrop-blur text-nowrap flex-none transition-colors duration-500 border-b border-slate-900/20 dark:border-slate-50/[0.2] bg-white/20 supports-backdrop-blur:bg-white/60 dark:bg-slate-900/80"
+      className="sticky top-0 rounded-tl-md rounded-tr-md z-40 w-full backdrop-blur text-nowrap flex-none transition-colors duration-500 border-b border-slate-900/20 dark:border-slate-50/[0.2] bg-white/80 supports-backdrop-blur:bg-white/60 dark:bg-slate-900/80"
       translate="no"
     >
       <Container className="flex items-center justify-between py-4">
