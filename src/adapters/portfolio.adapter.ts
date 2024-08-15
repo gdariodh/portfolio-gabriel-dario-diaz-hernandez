8e54e127ee80b43d23f767924f47f37404615111
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2024-08-14 22:57:53 -0400

    refactor: refactor portfolio adapter

--- src/adapters/portfolio.adapter.ts
+++ src/adapters/portfolio.adapter.ts
@@ -6,6 +6,49 @@ import {
   tagsAdapter,
 } from '@/adapters';
 
+export const socialMediaAdapter = (data: any[]) => {
+  return data.map((social: any) => {
+    return {
+      name: singleParagraphAdapter(social.name_social),
+      link: social.link_social,
+    };
+  });
+};
+
+export const workExperiencesAdapter = (data: any[]) => {
+  return data.map((experience: any) => {
+    const tags = tagsAdapter(multiParagraphAdapter(experience.tags_experience));
+    return {
+      title: singleParagraphAdapter(experience.title_experience),
+      text: multiParagraphAdapter(experience.text_experience),
+      time: singleParagraphAdapter(experience.time_experience),
+      link: experience.link_experience,
+      altLink: experience.alt_link_experience,
+      tags,
+      image: imageAdapter(experience.image_experience),
+      slug: experience.id_experience,
+      type: 'work-experience',
+    };
+  });
+};
+
+export const personalProjectsAdapter = (data: any[]) => {
+  return data.map((project: any) => {
+    const tags = tagsAdapter(multiParagraphAdapter(project.tags_project));
+    return {
+      title: singleParagraphAdapter(project.title_project),
+      text: multiParagraphAdapter(project.text_project),
+      link: project.link_project,
+      altLink: project.alt_link_project,
+      codeLink: project.code_link_project,
+      tags,
+      image: imageAdapter(project.image_project),
+      slug: project.id_project,
+      type: 'personal-project',
+    };
+  });
+};
+
 export const portfolioAdapter = (data: any) => {
   return {
     name: singleParagraphAdapter(data.name),
@@ -16,42 +59,9 @@ export const portfolioAdapter = (data: any) => {
     resumeLink: data.resume_link,
     location: singleParagraphAdapter(data.location),
     email: singleParagraphAdapter(data.email),
-    socialMedia: data.social_media.map((social: any) => {
-      return {
-        name: singleParagraphAdapter(social.name_social),
-        link: social.link_social,
-      };
-    }),
-    workExperiences: data.experiences.map((experience: any) => {
-      const tags = tagsAdapter(
-        multiParagraphAdapter(experience.tags_experience)
-      );
-      return {
-        title: singleParagraphAdapter(experience.title_experience),
-        text: multiParagraphAdapter(experience.text_experience),
-        time: singleParagraphAdapter(experience.time_experience),
-        link: experience.link_experience,
-        altLink: experience.alt_link_experience,
-        tags,
-        image: imageAdapter(experience.image_experience),
-        slug: experience.id_experience,
-        type: 'work-experience',
-      };
-    }),
+    socialMedia: socialMediaAdapter(data.social_media),
+    workExperiences: workExperiencesAdapter(data.experiences),
     showPersonalProjects: data.show_personal_projects,
-    personalProjects: data.projects.map((project: any) => {
-      const tags = tagsAdapter(multiParagraphAdapter(project.tags_project));
-      return {
-        title: singleParagraphAdapter(project.title_project),
-        text: multiParagraphAdapter(project.text_project),
-        link: project.link_project,
-        altLink: project.alt_link_project,
-        codeLink: project.code_link_project,
-        tags,
-        image: imageAdapter(project.image_project),
-        slug: project.id_project,
-        type: 'personal-project',
-      };
-    }),
+    personalProjects: personalProjectsAdapter(data.projects),
   } as Portfolio;
 };
--- src/models/portfolio.model.ts
+++ src/models/portfolio.model.ts
@@ -3,35 +3,35 @@ import { LinkPrismic, ImagePrismic } from '@/models/prismic.model';
 export type ExperienceType = 'work-experience' | 'personal-project';
 
 export interface Experience {
-  title: string;
-  text: string[];
-  image?: ImagePrismic;
-  link?: LinkPrismic;
   altLink?: LinkPrismic;
   codeLink?: LinkPrismic;
-  tags?: string[];
-  time?: string;
+  image?: ImagePrismic;
+  link?: LinkPrismic;
   slug: string;
+  tags?: string[];
+  text: string[];
+  time?: string;
+  title: string;
   type: ExperienceType;
 }
 
 export interface SocialMedia {
-  name?: string;
-  link: LinkPrismic;
   label?: string;
+  link: LinkPrismic;
+  name?: string;
 }
 
 export interface Portfolio {
+  email: string;
+  image: ImagePrismic;
+  location: string;
   name: string;
+  personalProjects: Experience[];
+  resumeLink: LinkPrismic;
   role: string;
-  text_short: string;
+  showPersonalProjects: boolean;
   socialMedia: any[];
   summary: string[];
+  text_short: string;
   workExperiences: Experience[];
-  personalProjects: Experience[];
-  image: ImagePrismic;
-  resumeLink: LinkPrismic;
-  location: string;
-  email: string;
-  showPersonalProjects: boolean;
 }
